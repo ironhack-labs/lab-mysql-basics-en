84d58dcc21ce63b67c57728bf7bfbd28968006cf
Jesus5657 <jesusdhernandez2020@gmail.com>
2024-12-06 17:51:37 -0400

    sql-basic

--- files_for_lab/Solution.sql
+++
@@ -1,5 +0,0 @@
-SELECT account_id, type, SUM(amount) AS total_amount
-FROM bank.trans
-WHERE account_id = 396
-GROUP BY type
-ORDER BY type;
--- files_for_lab/Solutions.sql
+++ files_for_lab/Solutions.sql
@@ -1,1 +1,5 @@
-Hello World
+SELECT account_id, type, SUM(amount) AS total_amount
+FROM bank.trans
+WHERE account_id = 396
+GROUP BY type
+ORDER BY type;
