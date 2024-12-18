0322708b501532d5ea3cf39e7142daa93cfcc305
psylviay <perez.sylvia.y@gmail.com>
2024-12-18 12:33:13 -0400

    Add files via upload

--- solutions.sql
+++ solutions.sql
@@ -1,7 +1,12 @@
-/* Challenge #1 - Who Published What At Where
-Uses MySQLs SELECT query that joins mulitiple tables to figure out
-what each author published at each publisher. Outcome is a data table.*/
-USE Tables;
+-- Check the currently selected database
+SELECT DATABASE();
+
+-- Use the correct database
+USE lab_sql_select;
+
+-- Challenge #1 - Who Published What At Where
+-- Uses MySQL SELECT query with multiple joins to figure out
+-- what each author published at each publisher.
 
 SELECT 
     a.au_id, 
@@ -12,12 +17,11 @@ SELECT
 FROM authors AS a
 INNER JOIN titleauthor AS ta ON a.au_id = ta.au_id
 INNER JOIN titles AS t ON ta.title_id = t.title_id
-INNER JOIN publishers AS p ON t.pub_id = p.pub_id
-ORDER BY a.au_titleauthorlname ASC, t.title ASC;
+INNER JOIN publishers AS p ON t.pub_id = p.pub_id;
 
 /* Challenge #2 - Who Have Published How Many At Where
 Queries how many titles each author has published at each publisher.*/
-USE Tables;
+-- USE lab_sql_select;
 
 SELECT 
     a.au_id, 
@@ -34,7 +38,7 @@ ORDER BY a.au_lname ASC, p.pub_name ASC;
 
 /* Challenge #3 - Best Selling Authors
 Finds the top 3 authors who have sold the highest number of titles. */
-USE Tables;
+-- USE Tables;
 
 SELECT 
     a.au_id, 
@@ -49,7 +53,7 @@ LIMIT 3;
 
 /* Challenge #4 - Best Selling Authors Ranking
 Instead of the top 3, list all 23 authors including those with 0 titles.*/
-USE Tables;
+-- USE Tables;
 
 SELECT 
     a.au_id, 
@@ -60,4 +64,3 @@ FROM authors AS a
 LEFT JOIN titleauthor AS ta ON a.au_id = ta.au_id
 GROUP BY a.au_id, a.au_lname, a.au_fname
 ORDER BY total_books DESC, a.au_lname ASC;
-
